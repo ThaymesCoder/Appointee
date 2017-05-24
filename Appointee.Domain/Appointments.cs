03ee57b8725ef3ffdbb42b9c503aeec4072d28a4
Thaymes <thamue1892@gmail.com>
2017-05-24 16:11:57 +0200

    Initial Commit -Appointee.Domain

---
+++ Appointee.Domain/Appointments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointee.Domain
+{
+  public class Appointments
+  {
+	public int Id { get; set; }
+	public DateTime Date { get; set; }
+	public string Location { get; set; }
+	public string Agenda { get; set; }
+  }
+}
